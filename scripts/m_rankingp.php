5e09ddf33ad00429b3488172fc33096bc2aac313
laussern <laussern@ariesgamers.net>
2013-05-24 21:12:56 -0430

    Add Ranking Player Coded

--- scripts/m_rankingp.php
+++ scripts/m_rankingp.php
@@ -1,3 +1,9 @@
+<!-- Hola Puto -->
+<?
+  $q = mssql_query("SELECT TOP 50 Name, Level FROM Character Where Name != '' Order by XP DESC");
+  if (mssql_num_rows($q)) {
+    $i = 1;
+?>
 <div class="span8 well" >
 	<h2>Ranking Individual</h2>
 	<table class="table table-condensed table-striped">
@@ -7,59 +13,18 @@
 			<td><strong>Experiencia</strong></td>
 			<td><strong>Nivel</strong></td>
 		</tr>
-		<tr>
-			<td>1</td>
-			<td>Laussern</td>
-			<td>929838784</td>
-			<td>99 lv.</td>
-		</tr>
-		<tr>
-			<td>3</td>
-			<td>Laussern</td>
-			<td>929838784</td>
-			<td>99 lv.</td>
-		</tr>
-		<tr>
-			<td>4</td>
-			<td>Laussern</td>
-			<td>929838784</td>
-			<td>99 lv.</td>
-		</tr>
-		<tr>
-			<td>5</td>
-			<td>Laussern</td>
-			<td>929838784</td>
-			<td>99 lv.</td>
-		</tr>
-		<tr>
-			<td>6</td>
-			<td>Laussern</td>
-			<td>929838784</td>
-			<td>99 lv.</td>
-		</tr>
-		<tr>
-			<td>7</td>
-			<td>Laussern</td>
-			<td>929838784</td>
-			<td>99 lv.</td>
-		</tr>
-		<tr>
-			<td>8</td>
-			<td>Laussern</td>
-			<td>929838784</td>
-			<td>99 lv.</td>
-		</tr>
-		<tr>
-			<td>9</td>
-			<td>Laussern</td>
-			<td>929838784</td>
-			<td>99 lv.</td>
-		</tr>
-		<tr>
-			<td>10</td>
-			<td>Laussern</td>
-			<td>929838784</td>
-			<td>99 lv.</td>
-		</tr>
+		<?
+          while ($r = mssql_fetch_object($q)) {
+        ?>
+        <tr>
+          <td><?=$i?></td>
+          <td><?=utf8_encode($r->Name)?></td>
+          <td><?=$r->XP?></td>
+          <td><?=$r->Level?></td>
+        </tr>
+        <?
+          $i++;
+          }
+        ?>
 	</table>
 </div>
